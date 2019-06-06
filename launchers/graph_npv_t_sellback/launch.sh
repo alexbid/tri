fcb6640a521506f8bd85933d83eeb6157bc4beb4
alexbid <alexbid@users.noreply.github.com>
2019-06-06 11:01:18 +0200

    Update launch.sh

--- launchers/graph_npv_t_sellback/launch.sh
+++ launchers/graph_npv_t_sellback/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ROOT=/Users/alex/tri/distribs/bin
-
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
 
 SELL_BACK="12 24 36 48 60 72 84 96 108 120 144 156 168 180 192 204 216 228 240 360"
 
@@ -10,7 +10,8 @@ do
 
 	# echo TUTU $
 	# ls -l ${ROOT}/../etc/eco.properties
-	sed -i".bak" "s/infine=.*/infine=${SELL}/g" ${ROOT}/../etc/eco.properties
+	sed -i".bak" "s/infine=.*/infine=${SELL}/g" ${SCRIPTPATH}/../../distribs/etc/eco.properties
 	# grep infine ${ROOT}/../etc/eco.properties
-	echo $SELL $(${ROOT}/buy.sh | grep NPV) $(${ROOT}/rent.sh | grep NPV)
+	echo $SELL $(${SCRIPTPATH}/../../distribs/bin/buy.sh | grep NPV) $(${SCRIPTPATH}/../../distribs/bin/rent.sh | grep NPV)
 done
+#
